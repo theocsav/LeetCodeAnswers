b410ed52c69c51b431afc622278009ffbe2e9bcb
Vasco Hinostroza <82005643+theocsav@users.noreply.github.com>
2025-03-01 23:24:02 -0500

    Time: 0 ms (100%), Space: 18.5 MB (27.02%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -12,31 +12,16 @@
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        string left = "";
-        string right = "";
-        inorder_left(root, root->val, left);
-        cout << left << endl;
-        cout << right << endl;
-        inorder_right(root, root->val, right);
-        return left == right;
+        return isMirror(root->left, root->right);
     }
 
-    void inorder_left(TreeNode* node, int head_value, string &left){
-        if(node == nullptr){
-            left.append("#")
-            return;
+    bool isMirror(TreeNode* left, TreeNode* right){
+        if(left == nullptr && right == nullptr){
+            return true;
         }
-        inorder_left(node->left, head_value, left);
-        left.append(to_string(node->val));
-        inorder_left(node->right, head_value, left);
-    }
-    void inorder_right(TreeNode* node, int head_value, string &right){
-        if(node == nullptr){
-            right.append("#")
-            return;
+        if(left == nullptr || right == nullptr || left->val != right->val){
+            return false;
         }
-        inorder_right(node->right, head_value, right);
-        right.append(to_string(node->val));
-        inorder_right(node->left, head_value, right);
+        return isMirror(left->left, right->right) && isMirror(left->right, right->left);
     }
 };
