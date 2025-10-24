9ad6ca9d0e48e164e253c5cb699473b3146d021c
Vasco Hinostroza <82005643+theocsav@users.noreply.github.com>
2025-10-23 23:34:20 -0500

    Time: 0 ms (100%), Space: 8 MB (92.28%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        int reversed = 0;
+
+        for(int i = 0; i < 32; i++){
+            reversed <<= 1;
+
+            if((n & 1) == 1){
+                reversed |= 1;
+            }
+            
+            n >>= 1;
+        }
+        return reversed;
+    }
+};
